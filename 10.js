45b1c3f2fd13cd6cf083041cba252162e8d332bd
alex0675638820 <netrebko2505@gmail.com>
2024-02-21 15:15:09 +0200

    train-24

--- 10.js
+++ 10.js
@@ -2,11 +2,27 @@ console.log("Завдання: 10 ==============================");
 
 function task10() {
   // Створюємо проміс promise1, які вирішуються через 500мс, та повертають як успішне виконання рядок "Promise 1".
+  let promise1 = new Promise((resolve) => 
+	setTimeout(resolve, 500, "Promise 1")
+  );
   // Створюємо проміс promise2, які вирішуються через 200мс, та повертають як успішне виконання рядок "Promise 2".
+  let promise2 = new Promise((resolve) => 
+	setTimeout(resolve, 200, "Promise 2")
+  );
   // Створюємо проміс promise3, які вирішуються через 300мс, та повертають як успішне виконання рядок "Promise 3".
+  let promise3 = new Promise((resolve) => 
+  setTimeout(resolve, 300, "Promise 3")
+);
   // Promise.any() приймає масив промісів які ми створили вище і вирішується, як тільки перший із цих промісів вирішується.
+Promise.any([promise1, promise2, promise3])
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
+.then((value) => {
+	console.log(value);
+})
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+.catch((error) => {
+	console.log(error);
+})
 }
 
 // Викликаємо функцію task10
